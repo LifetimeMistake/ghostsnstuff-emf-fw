e2eca840d34fb70ddda70eb2695e6747cece386f
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2024-10-20 18:40:15 +0200

    Add experimental IMU implementation

---
+++ src/imu.rs
@@ -0,0 +1,333 @@
+use std::{f32::consts::PI, fmt::Debug};
+
+use bmi160::{
+    interface::{ReadData, WriteData},
+    Bmi160, Error, Sensor3DDataScaled, SensorSelector,
+};
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Axis {
+    X,
+    Y,
+    Z,
+}
+
+#[derive(Debug)]
+pub struct IMUMeasurement {
+    pub raw_accel_x: f32,
+    pub raw_accel_y: f32,
+    pub raw_accel_z: f32,
+    pub raw_gyro_x: f32,
+    pub raw_gyro_y: f32,
+    pub raw_gyro_z: f32,
+    pub pitch: f32,
+    pub yaw: f32,
+    pub roll: f32,
+    pub gravity_x: f32,
+    pub gravity_y: f32,
+    pub gravity_z: f32,
+    pub acceleration_x: f32,
+    pub acceleration_y: f32,
+    pub acceleration_z: f32,
+    pub velocity_x: f32,
+    pub velocity_y: f32,
+    pub velocity_z: f32,
+    pub position_x: f32,
+    pub position_y: f32,
+    pub position_z: f32,
+}
+
+pub struct AxisMapping {
+    pub x: (Axis, bool), // (Physical Axis, Invert)
+    pub y: (Axis, bool),
+    pub z: (Axis, bool),
+}
+
+const GRAVITY: f32 = 9.81;
+
+fn get_mapped_axis(data: &Sensor3DDataScaled, mapping: &(Axis, bool)) -> f32 {
+    let value = match mapping.0 {
+        Axis::X => data.x,
+        Axis::Y => data.y,
+        Axis::Z => data.z,
+    };
+
+    if mapping.1 {
+        -value
+    } else {
+        value
+    }
+}
+
+fn remap_axes(data: &Sensor3DDataScaled, mapping: &AxisMapping) -> Sensor3DDataScaled {
+    Sensor3DDataScaled {
+        x: get_mapped_axis(data, &mapping.x),
+        y: get_mapped_axis(data, &mapping.y),
+        z: get_mapped_axis(data, &mapping.z),
+    }
+}
+
+// Function to rotate a vector using pitch, roll, and yaw
+fn rotate_vector(x: f32, y: f32, z: f32, pitch: f32, roll: f32, yaw: f32) -> (f32, f32, f32) {
+    let cos_pitch = pitch.cos();
+    let sin_pitch = pitch.sin();
+    let cos_roll = roll.cos();
+    let sin_roll = roll.sin();
+    let cos_yaw = yaw.cos();
+    let sin_yaw = yaw.sin();
+
+    // Rotation matrix components
+    let r11 = cos_yaw * cos_pitch;
+    let r12 = cos_yaw * sin_pitch * sin_roll - sin_yaw * cos_roll;
+    let r13 = cos_yaw * sin_pitch * cos_roll + sin_yaw * sin_roll;
+    let r21 = sin_yaw * cos_pitch;
+    let r22 = sin_yaw * sin_pitch * sin_roll + cos_yaw * cos_roll;
+    let r23 = sin_yaw * sin_pitch * cos_roll - cos_yaw * sin_roll;
+    let r31 = -sin_pitch;
+    let r32 = cos_pitch * sin_roll;
+    let r33 = cos_pitch * cos_roll;
+
+    // Apply rotation
+    let new_x = r11 * x + r12 * y + r13 * z;
+    let new_y = r21 * x + r22 * y + r23 * z;
+    let new_z = r31 * x + r32 * y + r33 * z;
+
+    (new_x, new_y, new_z)
+}
+
+pub struct IMU<T, CommE>
+where
+    T: ReadData<Error = Error<CommE>> + WriteData<Error = Error<CommE>>,
+    CommE: Debug,
+{
+    imu: Bmi160<T>,
+    accel_mapping: Option<AxisMapping>,
+    gyro_mapping: Option<AxisMapping>,
+    prev_time: u32,
+    pitch: f32,
+    roll: f32,
+    yaw: f32,
+    bias_x: f32,
+    bias_y: f32,
+    bias_z: f32,
+    alpha: f32,
+    velocity_x: f32,
+    velocity_y: f32,
+    velocity_z: f32,
+    position_x: f32,
+    position_y: f32,
+    position_z: f32,
+    is_calibrated: bool
+}
+
+impl<T, CommE> IMU<T, CommE>
+where
+    T: ReadData<Error = Error<CommE>> + WriteData<Error = Error<CommE>>,
+    CommE: Debug,
+{
+    pub fn new(
+        imu: Bmi160<T>,
+        accel_mapping: Option<AxisMapping>,
+        gyro_mapping: Option<AxisMapping>,
+        alpha: Option<f32>,
+    ) -> Self {
+        Self {
+            imu,
+            accel_mapping,
+            gyro_mapping,
+            prev_time: 0,
+            pitch: 0.0,
+            roll: 0.0,
+            yaw: 0.0,
+            bias_x: 0.0,
+            bias_y: 0.0,
+            bias_z: 0.0,
+            alpha: alpha.unwrap_or(0.9),
+            position_x: 0.0,
+            position_y: 0.0,
+            position_z: 0.0,
+            velocity_x: 0.0,
+            velocity_y: 0.0,
+            velocity_z: 0.0,
+            is_calibrated: false
+        }
+    }
+
+    fn read_data(&mut self, apply_bias: bool) -> Result<IMUMeasurement, Error<CommE>> {
+        let selector = SensorSelector::new().accel().gyro().time();
+        let data = self.imu.data_scaled(selector)?;
+        let accel_raw = data.accel.unwrap();
+        let gyro_raw = data.gyro.unwrap();
+        let current_time = data.time.unwrap();
+
+        let accel;
+        let gyro;
+
+        if self.accel_mapping.is_some() {
+            accel = remap_axes(&accel_raw, self.accel_mapping.as_ref().unwrap());
+        } else {
+            accel = accel_raw;
+        }
+
+        if self.gyro_mapping.is_some() {
+            gyro = remap_axes(&gyro_raw, self.gyro_mapping.as_ref().unwrap());
+        } else {
+            gyro = gyro_raw;
+        }
+
+        // Calculate delta time (dt) in seconds
+        let dt_us = if current_time < self.prev_time {
+            (current_time as i64 + (1 << 24) - self.prev_time as i64) * 39
+        } else {
+            (current_time as i64 - self.prev_time as i64) * 39
+        };
+        let dt = (dt_us as f32) / 1_000_000.0;
+        self.prev_time = current_time;
+
+        let gyro_x = gyro.x * (PI / 180.0);
+        let gyro_y = gyro.y * (PI / 180.0);
+        let gyro_z = gyro.z * (PI / 180.0);
+
+        let accel_x = accel.x * GRAVITY;
+        let accel_y = accel.y * GRAVITY;
+        let accel_z = accel.z * GRAVITY;
+
+        // Calculate pitch and roll from accelerometer
+        let pitch_acc = (accel_y / (accel_x.powi(2) + accel_z.powi(2)).sqrt()).atan();
+        let roll_acc = (-accel_x / (accel_z.powi(2) + accel_y.powi(2)).sqrt()).atan();
+
+        // Apply complementary filter
+        self.pitch = self.alpha * (self.pitch + gyro_y * dt) + (1.0 - self.alpha) * pitch_acc;
+        self.roll = self.alpha * (self.roll + gyro_x * dt) + (1.0 - self.alpha) * roll_acc;
+        self.yaw += gyro_z * dt; // yaw is accumulated without filtering
+
+        // Rotate the gravity vector according to the orientation
+        let cos_pitch = self.pitch.cos();
+        let sin_pitch = self.pitch.sin();
+        let cos_roll = self.roll.cos();
+        let sin_roll = self.roll.sin();
+
+        let gravity_comp_x = GRAVITY * sin_pitch;
+        let gravity_comp_y = GRAVITY * sin_roll;
+        let gravity_comp_z = GRAVITY * (cos_pitch * cos_roll);
+
+        let (bias_x, bias_y, bias_z);
+        if apply_bias {
+            (bias_x, bias_y, bias_z) = rotate_vector(
+                self.bias_x, 
+                self.bias_y, 
+                self.bias_z, 
+                self.pitch, 
+                self.roll, 
+                self.yaw
+            );
+        } else {
+            (bias_x, bias_y, bias_z) = (0.0, 0.0, 0.0);
+        }
+
+        // Apply gravity compensation to accelerometer readings
+        let acceleration_x = (accel_x - gravity_comp_x - bias_x) * dt;
+        let acceleration_y = (accel_y - gravity_comp_y - bias_y) * dt;
+        let acceleration_z = (accel_z - gravity_comp_z - bias_z) * dt;
+
+        if (self.is_calibrated) {
+            // Integrate acceleration to update velocity
+            self.velocity_x += acceleration_x;
+            self.velocity_y += acceleration_y;
+            self.velocity_z += acceleration_z;
+
+            // Integrate velocity to update position
+            self.position_x += self.velocity_x * dt;
+            self.position_y += self.velocity_y * dt;
+            self.position_z += self.velocity_z * dt;
+        }
+
+        let result = IMUMeasurement {
+            raw_accel_x: accel_x,
+            raw_accel_y: accel_y,
+            raw_accel_z: accel_z,
+            raw_gyro_x: gyro_x,
+            raw_gyro_y: gyro_y,
+            raw_gyro_z: gyro_z,
+            pitch: self.pitch,
+            yaw: self.yaw,
+            roll: self.roll,
+            gravity_x: gravity_comp_x,
+            gravity_y: gravity_comp_y,
+            gravity_z: gravity_comp_z,
+            acceleration_x: acceleration_x,
+            acceleration_y: acceleration_y,
+            acceleration_z: acceleration_z,
+            velocity_x: self.velocity_x,
+            velocity_y: self.velocity_y,
+            velocity_z: self.velocity_z,
+            position_x: self.position_x,
+            position_y: self.position_y,
+            position_z: self.position_z
+        };
+
+        Ok(result)
+    }
+
+    pub fn data(&mut self) -> Result<IMUMeasurement, Error<CommE>> {
+        if (!self.is_calibrated) {
+            self.calibrate(500)?;
+        }
+        self.read_data(true)
+    }
+
+    pub fn reset_position(&mut self) {
+        self.position_x = 0.0;
+        self.position_y = 0.0;
+        self.position_z = 0.0;
+        self.velocity_x = 0.0;
+        self.velocity_y = 0.0;
+        self.velocity_z = 0.0;
+    }
+
+    pub fn calibrate(&mut self, samples: u32) -> Result<(f32, f32, f32), Error<CommE>> {
+        let mut accel_measurements: Vec<(f32, f32, f32)> = Vec::new();
+        let mut gyro_measurements: Vec<(f32, f32, f32)> = Vec::new();
+
+        for _ in 0..samples {
+            let m = self.read_data(false)?;
+            accel_measurements.push((m.raw_accel_x, m.raw_accel_y, m.raw_accel_z - GRAVITY));
+            gyro_measurements.push((m.raw_gyro_x, m.raw_gyro_y, m.raw_gyro_z));
+        }
+
+        let (abx, aby, abz): (f32, f32, f32) = accel_measurements
+            .iter()
+            .cloned()
+            .fold((0.0, 0.0, 0.0), |(sum_x, sum_y, sum_z), (x, y, z)| {
+                (sum_x + x, sum_y + y, sum_z + z)
+            });
+
+        let (gbx, gby, gbz): (f32, f32, f32) = gyro_measurements
+            .iter()
+            .cloned()
+            .fold((0.0, 0.0, 0.0), |(sum_x, sum_y, sum_z), (x, y, z)| {
+                (sum_x + x, sum_y + y, sum_z + z)
+            });
+
+        let accel_bias_x: f32 = abx / accel_measurements.len() as f32;
+        let accel_bias_y: f32 = aby / accel_measurements.len() as f32;
+        let accel_bias_z: f32 = abz / accel_measurements.len() as f32;
+        let avg_gyro_x: f32 = gbx / gyro_measurements.len() as f32;
+        let avg_gyro_y: f32 = gby / gyro_measurements.len() as f32;
+        let avg_gyro_z: f32 = gbz / gyro_measurements.len() as f32;
+
+        (self.bias_x, self.bias_y, self.bias_z) = rotate_vector(
+            accel_bias_x,
+            accel_bias_y,
+            accel_bias_z,
+            -avg_gyro_y,
+            -avg_gyro_x,
+            -avg_gyro_z
+        );
+
+        self.is_calibrated = true;
+        Ok((self.bias_x, self.bias_y, self.bias_z))
+    }
+
+    // pub fn get_heading(&mut self) -> Result<(f32, f32, f32)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 #![feature(const_float_methods)]
 #![allow(non_snake_case)]
 
+pub mod imu;
 pub mod led;
 pub mod speaker;
 pub mod utils;
 
+use std::thread::sleep;
+use std::time::Duration;
+
+use bmi160::{AccelerometerRange, Bmi160, GyroscopeRange, SlaveAddr};
+use esp_idf_hal::i2c::{I2cConfig, I2cDriver};
 use esp_idf_hal::ledc::{self, config::TimerConfig, LedcDriver, LedcTimerDriver};
 use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_hal::rmt::config::TransmitConfig;
 use esp_idf_hal::rmt::TxRmtDriver;
 
+use esp_idf_hal::units::FromValueType;
+use imu::{Axis, AxisMapping, IMU};
 use led::{get_emf_colors, LEDOrder, Ws2812};
 use speaker::{get_emf_pcm, PWMSpeaker};
 use utils::ledc_resolution_to_u32;
@@ -23,44 +31,113 @@ fn main() {
     let SPEAKER_PIN = peripherals.pins.gpio8;
     let SPEAKER_RESOLUTION = ledc::Resolution::Bits8;
     let SAMPLE_RATE = 8000;
-    let SAMPLE_DURATION_MS = 1000;
+    let SAMPLE_DURATION_MS = 3000;
     let LED_PIN = peripherals.pins.gpio5;
     let LED_ORDER = LEDOrder::Reverse;
+    let IMU_I2C_DEVICE = peripherals.i2c0;
+    let IMU_I2C_SDA_PIN = peripherals.pins.gpio6;
+    let IMU_I2C_SCL_PIN = peripherals.pins.gpio7;
+    let IMU_I2C_BAUDRATE = 100_u32.kHz();
+    let IMU_ACCEL_RANGE = AccelerometerRange::G4;
+    let IMU_ACCEL_MAP = AxisMapping {
+        x: (Axis::Y, false),
+        y: (Axis::X, true),
+        z: (Axis::Z, false),
+    };
+    let IMU_GYRO_RANGE = GyroscopeRange::Scale1000;
+    let IMU_GYRO_MAP = AxisMapping {
+        x: (Axis::Y, false),
+        y: (Axis::X, true),
+        z: (Axis::Z, true),
+    };
+    let IMU_ALPHA: f32 = 0.9;
 
     // Speaker init code
     let mut speaker_timer = LedcTimerDriver::new(
         peripherals.ledc.timer0,
-        &(TimerConfig::new()
-        .resolution(SPEAKER_RESOLUTION))
-    ).unwrap();
-    let mut speaker_driver = LedcDriver::new(
-        peripherals.ledc.channel0,
-        &speaker_timer,
-        SPEAKER_PIN
-    ).unwrap();
+        &(TimerConfig::new().resolution(SPEAKER_RESOLUTION)),
+    )
+    .unwrap();
+    let mut speaker_driver =
+        LedcDriver::new(peripherals.ledc.channel0, &speaker_timer, SPEAKER_PIN).unwrap();
     let mut speaker = PWMSpeaker::new(
-        &mut speaker_driver, 
-        &mut speaker_timer, 
-        ledc_resolution_to_u32(SPEAKER_RESOLUTION)
+        &mut speaker_driver,
+        &mut speaker_timer,
+        ledc_resolution_to_u32(SPEAKER_RESOLUTION),
     );
 
     // LED init code
-    let mut led_tx = TxRmtDriver::new( 
-        peripherals.rmt.channel0, 
-        LED_PIN, 
-        &TransmitConfig::new().clock_divider(1)
-    ).unwrap();
+    let mut led_tx = TxRmtDriver::new(
+        peripherals.rmt.channel0,
+        LED_PIN,
+        &TransmitConfig::new().clock_divider(1),
+    )
+    .unwrap();
     let mut led = Ws2812::new(&mut led_tx, 5);
 
+    // IMU init code
+    let imu_i2c = I2cDriver::new(
+        IMU_I2C_DEVICE,
+        IMU_I2C_SDA_PIN,
+        IMU_I2C_SCL_PIN,
+        &I2cConfig::new().baudrate(IMU_I2C_BAUDRATE.into()),
+    )
+    .unwrap();
+    let mut imu_driver = Bmi160::new_with_i2c(imu_i2c, SlaveAddr::Alternative(true));
+
+    println!("IMU SETUP");
+    println!("Chip ID: {}", imu_driver.chip_id().unwrap());
+
+    imu_driver.set_accel_power_mode(bmi160::AccelerometerPowerMode::Normal)
+        .unwrap();
+    imu_driver.set_accel_range(IMU_ACCEL_RANGE).unwrap();
+    imu_driver.set_gyro_power_mode(bmi160::GyroscopePowerMode::Normal)
+        .unwrap();
+    imu_driver.set_gyro_range(IMU_GYRO_RANGE).unwrap();
+
+    println!("COMPLETE");
+    println!("Status: {:?}", imu_driver.status().unwrap());
+    println!("Power mode: {:?}", imu_driver.power_mode().unwrap());
+
+    let mut imu = IMU::new(
+        imu_driver, 
+        Some(IMU_ACCEL_MAP), 
+        Some(IMU_GYRO_MAP), 
+        Some(IMU_ALPHA)
+    );
+
+    let (bias_x, bias_y, bias_z) = imu.calibrate(500).unwrap();
+    println!(
+        "computed sensor bias: {:.2}, {:.2}, {:.2}",
+        bias_x, bias_y, bias_z
+    );
+
+    loop {
+        let measurement: imu::IMUMeasurement = imu.data().unwrap();
+        println!(
+            "Px: {:.2}, Py: {:.2}, Pz: {:.2}",
+            // measurement.pitch,
+            // measurement.roll,
+            // measurement.yaw,
+            measurement.position_x,
+            measurement.position_y,
+            measurement.position_z
+        );
+        sleep(Duration::from_millis(10));
+    }
+
     for _ in 0..3 {
-        for level in 1..6 {
+        for level in 1..7 {
             let mut colors = get_emf_colors(level);
             if LED_ORDER == LEDOrder::Reverse {
                 colors.reverse();
             }
-            let pcm_data = get_emf_pcm(level, SAMPLE_RATE, SAMPLE_DURATION_MS).unwrap();
             led.set_colors(&colors).unwrap();
-            speaker.play_pcm(&pcm_data, SAMPLE_RATE).unwrap();
+
+            if level >= 2 {
+                let pcm_data = get_emf_pcm(level, SAMPLE_RATE, SAMPLE_DURATION_MS).unwrap();
+                speaker.play_pcm(&pcm_data, SAMPLE_RATE).unwrap();
+            }
         }
     }
 
