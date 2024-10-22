36794339bb2669830913bcdc093e97fa68d260a2
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2024-10-22 22:58:12 +0200

    Now periodically re-calibrate IMU

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,16 @@ pub mod led;
 pub mod emf;
 pub mod speaker;
 
+use std::fmt::Debug;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 
-use bmi160::{AccelerometerRange, Bmi160, GyroscopeRange, SlaveAddr};
+use bmi160::interface::{ReadData, WriteData};
+use bmi160::{AccelerometerRange, Bmi160, Error, GyroscopeRange, SlaveAddr};
 use emf::{EMFReader, Ghost, User};
 use esp_idf_hal::i2c::{I2cConfig, I2cDriver};
-use esp_idf_hal::ledc::{self, config::TimerConfig, LedcDriver, LedcTimerDriver};
+use esp_idf_hal::ledc::{LedcTimer, Resolution};
+use esp_idf_hal::ledc::{config::TimerConfig, LedcDriver, LedcTimerDriver};
 use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_hal::rmt::config::TransmitConfig;
 use esp_idf_hal::rmt::TxRmtDriver;
@@ -26,83 +29,35 @@ use speaker::{get_emf_pcm, PWMSpeaker};
 use utils::{ledc_resolution_to_u32, vectors_almost_equal, ERROR_TUNE_NOTES, SLEEP_TUNE_NOTES, SUCCESS_TUNE_NOTES, WAKEUP_TUNE_NOTES};
 use vector::Vector3;
 
-fn main() {
-    esp_idf_svc::sys::link_patches();
-    esp_idf_svc::log::EspLogger::initialize_default();
-    let peripherals = Peripherals::take().unwrap();
+// Constants
+const SPEAKER_RESOLUTION: Resolution = Resolution::Bits8;
+const LED_ORDER: LEDOrder = LEDOrder::Reverse;
+const SAMPLE_RATE: u32 = 8000;
+const SAMPLE_DURATION_MS: u32 = 10;
+const IMU_I2C_BAUDRATE_KHZ: u32 = 100;
+const IMU_ACCEL_RANGE: AccelerometerRange = AccelerometerRange::G4;
+const IMU_ACCEL_MAP: AxisMapping = AxisMapping {
+    x: (Axis::X, false),
+    y: (Axis::Y, true),
+    z: (Axis::Z, false),
+};
+const IMU_GYRO_RANGE: GyroscopeRange = GyroscopeRange::Scale1000;
+const IMU_GYRO_MAP: AxisMapping = AxisMapping {
+    x: (Axis::X, true),
+    y: (Axis::Y, true),
+    z: (Axis::Z, true),
+};
+const IMU_ALPHA: f32 = 0.9;
+const IMU_CALIBRATION_SAMPLES: u32 = 1000;
+const IMU_CALIBRATION_INTERVAL: Duration = Duration::from_secs(600);
+const SLEEP_TIMEOUT: Duration = Duration::from_secs(10);
+const WAKE_CHECK_INTERVAL: Duration = Duration::from_secs(1);
 
-    // Constants
-    let SPEAKER_PIN = peripherals.pins.gpio8;
-    let SPEAKER_RESOLUTION = ledc::Resolution::Bits8;
-    let SAMPLE_RATE = 8000;
-    let SAMPLE_DURATION_MS = 10;
-    let LED_PIN = peripherals.pins.gpio5;
-    let LED_ORDER = LEDOrder::Reverse;
-    let IMU_I2C_DEVICE = peripherals.i2c0;
-    let IMU_I2C_SDA_PIN = peripherals.pins.gpio6;
-    let IMU_I2C_SCL_PIN = peripherals.pins.gpio7;
-    let IMU_I2C_BAUDRATE = 100_u32.kHz();
-    let IMU_ACCEL_RANGE = AccelerometerRange::G4;
-    let IMU_ACCEL_MAP = AxisMapping {
-        x: (Axis::X, false),
-        y: (Axis::Y, true),
-        z: (Axis::Z, false),
-    };
-    let IMU_GYRO_RANGE = GyroscopeRange::Scale1000;
-    let IMU_GYRO_MAP = AxisMapping {
-        x: (Axis::X, true),
-        y: (Axis::Y, true),
-        z: (Axis::Z, true),
-    };
-    let IMU_ALPHA: f32 = 0.9;
-    let IMU_CALIBRATION_SAMPLES: u32 = 1000;
-    let SLEEP_TIMEOUT = Duration::from_secs(10);
-    let WAKE_CHECK_INTERVAL = Duration::from_secs(1);
-
-    // Speaker init code
-    let mut speaker_timer = LedcTimerDriver::new(
-        peripherals.ledc.timer0,
-        &(TimerConfig::new().resolution(SPEAKER_RESOLUTION)),
-    )
-    .unwrap();
-    let mut speaker_driver =
-        LedcDriver::new(peripherals.ledc.channel0, &speaker_timer, SPEAKER_PIN).unwrap();
-    let mut speaker = PWMSpeaker::new(
-        &mut speaker_driver,
-        &mut speaker_timer,
-        ledc_resolution_to_u32(SPEAKER_RESOLUTION),
-    );
-
-    // LED init code
-    let mut led_tx = TxRmtDriver::new(
-        peripherals.rmt.channel0,
-        LED_PIN,
-        &TransmitConfig::new().clock_divider(1),
-    )
-    .unwrap();
-    let mut led = Ws2812::new(&mut led_tx, 5);
-
-    // IMU init code
-    let imu_i2c = I2cDriver::new(
-        IMU_I2C_DEVICE,
-        IMU_I2C_SDA_PIN,
-        IMU_I2C_SCL_PIN,
-        &I2cConfig::new().baudrate(IMU_I2C_BAUDRATE.into()),
-    )
-    .unwrap();
-    let mut imu_driver = Bmi160::new_with_i2c(imu_i2c, SlaveAddr::Alternative(true));
-    imu_driver.set_accel_power_mode(bmi160::AccelerometerPowerMode::Normal).unwrap();
-    imu_driver.set_accel_range(IMU_ACCEL_RANGE).unwrap();
-    imu_driver.set_gyro_power_mode(bmi160::GyroscopePowerMode::Normal).unwrap();
-    imu_driver.set_gyro_range(IMU_GYRO_RANGE).unwrap();
-
-    let mut imu = IMU::new(
-        imu_driver, 
-        Some(IMU_ACCEL_MAP), 
-        Some(IMU_GYRO_MAP), 
-        Some(IMU_ALPHA)
-    );
-
+fn calibrate<T, W, E>(led: &mut Ws2812, speaker: &mut PWMSpeaker<T>, imu: &mut IMU<W, E>)
+where T : LedcTimer,
+W: ReadData<Error = Error<E>> + WriteData<Error = Error<E>>,
+E: Debug
+{
     loop {
         // Attempt to calibrate IMU
         println!("Calibrating IMU");
@@ -133,6 +88,63 @@ fn main() {
     speaker.play_tune(&SUCCESS_TUNE_NOTES, SAMPLE_RATE).unwrap();
 
     led.turn_off().unwrap();
+}
+
+fn main() {
+    esp_idf_svc::sys::link_patches();
+    esp_idf_svc::log::EspLogger::initialize_default();
+    let peripherals = Peripherals::take().unwrap();
+
+    // GPIO Constants
+    let SPEAKER_PIN = peripherals.pins.gpio8;
+    let LED_PIN = peripherals.pins.gpio5;
+    let IMU_I2C_DEVICE = peripherals.i2c0;
+    let IMU_I2C_SDA_PIN = peripherals.pins.gpio6;
+    let IMU_I2C_SCL_PIN = peripherals.pins.gpio7;
+
+    // Speaker init code
+    let mut speaker_timer = LedcTimerDriver::new(
+        peripherals.ledc.timer0,
+        &(TimerConfig::new().resolution(SPEAKER_RESOLUTION)),
+    )
+    .unwrap();
+    let mut speaker_driver =
+        LedcDriver::new(peripherals.ledc.channel0, &speaker_timer, SPEAKER_PIN).unwrap();
+    let mut speaker = PWMSpeaker::new(
+        &mut speaker_driver,
+        &mut speaker_timer,
+        ledc_resolution_to_u32(SPEAKER_RESOLUTION),
+    );
+
+    // LED init code
+    let mut led_tx = TxRmtDriver::new(
+        peripherals.rmt.channel0,
+        LED_PIN,
+        &TransmitConfig::new().clock_divider(1),
+    )
+    .unwrap();
+    let mut led = Ws2812::new(&mut led_tx, 5);
+
+    // IMU init code
+    let imu_i2c = I2cDriver::new(
+        IMU_I2C_DEVICE,
+        IMU_I2C_SDA_PIN,
+        IMU_I2C_SCL_PIN,
+        &I2cConfig::new().baudrate(IMU_I2C_BAUDRATE_KHZ.kHz().into()),
+    )
+    .unwrap();
+    let mut imu_driver = Bmi160::new_with_i2c(imu_i2c, SlaveAddr::Alternative(true));
+    imu_driver.set_accel_power_mode(bmi160::AccelerometerPowerMode::Normal).unwrap();
+    imu_driver.set_accel_range(IMU_ACCEL_RANGE).unwrap();
+    imu_driver.set_gyro_power_mode(bmi160::GyroscopePowerMode::Normal).unwrap();
+    imu_driver.set_gyro_range(IMU_GYRO_RANGE).unwrap();
+
+    let mut imu = IMU::new(
+        imu_driver, 
+        Some(IMU_ACCEL_MAP), 
+        Some(IMU_GYRO_MAP), 
+        Some(IMU_ALPHA)
+    );
 
     let mut emf = EMFReader::new(
         Ghost::new(
@@ -164,10 +176,18 @@ fn main() {
         get_emf_colors(6, LED_ORDER),
     ];
 
+    // Perform initial IMU calibration
+    calibrate(
+        &mut led,
+        &mut speaker,
+        &mut imu
+    );
+
     emf.activity_level = 4;
     let mut prev_level = 0;
     let mut last_heartbeat = Instant::now();
     let mut last_orientation_change = Instant::now();
+    let mut last_calibration = Instant::now();
     let mut is_sleeping = false;
     let mut last_orientation = Vector3::new(0.0, 0.0, 0.0);
 
@@ -196,6 +216,17 @@ fn main() {
             last_orientation_change = now;
             is_sleeping = false;
             prev_level = 0;
+
+            if now.duration_since(last_calibration) > IMU_CALIBRATION_INTERVAL {
+                // Prompt for re-calibration
+                calibrate(
+                    &mut led,
+                    &mut speaker,
+                    &mut imu
+                );
+
+                last_calibration = now;
+            }
         }
 
         emf.update_user(
